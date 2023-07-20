51c2e69f30ac353416f9b2877031aaefb1df372c
Jserin <jsr3643@naver.com>
2023-07-20 20:00:19 +0900

    first commit

---
+++ src/main/java/com/ll/jigumiyak/JigumiyakApplication.java
@@ -0,0 +1,13 @@
+package com.ll.jigumiyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigumiyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JigumiyakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/jigumiyak/JigumiyakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jigumiyak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigumiyakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
