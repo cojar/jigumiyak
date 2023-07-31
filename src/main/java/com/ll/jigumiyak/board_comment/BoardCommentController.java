a3ff2edcda48ab844355b355e00ca5070341e496
Jserin <jsr3643@naver.com>
2023-08-01 01:49:18 +0900

    comment->bcomment 수정

--- src/main/java/com/ll/jigumiyak/board_comment/BoardCommentController.java
+++ src/main/java/com/ll/jigumiyak/board_comment/BoardCommentController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
-@RequestMapping("/comment")
+@RequestMapping("/bcomment")
 @RequiredArgsConstructor
 @Controller
 public class BoardCommentController {
