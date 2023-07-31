5316b739d3a42f080316ff8aa9666b7abf526afd
pintor.dev <pintor.dev@gmail.com>
2023-08-01 00:34:52 +0900

    Update NutrientRepository.java

--- src/main/java/com/ll/jigumiyak/nutrient/NutrientRepository.java
+++ src/main/java/com/ll/jigumiyak/nutrient/NutrientRepository.java
@@ -3,6 +3,5 @@ package com.ll.jigumiyak.ksyTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface NutrientRepository extends JpaRepository<Nutrient, Long> {
 }
