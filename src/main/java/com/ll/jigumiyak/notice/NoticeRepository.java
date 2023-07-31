22642c75f2048ea3a8de2b15dee16d9dd27da2c8
pintordev <pintor.dev@gmail.com>
2023-08-01 00:58:45 +0900

    query skip

--- src/main/java/com/ll/jigumiyak/notice/NoticeRepository.java
+++ src/main/java/com/ll/jigumiyak/notice/NoticeRepository.java
@@ -7,31 +7,31 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
     // 키워드와 카테고리 둘 다 주어질때
-    @Query("SELECT n FROM Notice n " +
-            "WHERE ((:keywordCategory = 'title' AND n.title LIKE %:keyword%) " +
-            "OR (:keywordCategory = 'content' AND n.content LIKE %:keyword%) " +
-            "OR (:keywordCategory = 'author' AND n.author LIKE %:keyword%) " +
-            "OR (:keywordCategory = 'title+content' AND (n.title LIKE %:keyword% OR n.content LIKE %:keyword%))) " +
-            "AND (n.category.name = :category)")
+//    @Query("SELECT n FROM Notice n " +
+//            "WHERE ((:keywordCategory = 'title' AND n.title LIKE %:keyword%) " +
+//            "OR (:keywordCategory = 'content' AND n.content LIKE %:keyword%) " +
+//            "OR (:keywordCategory = 'author' AND n.author LIKE %:keyword%) " +
+//            "OR (:keywordCategory = 'title+content' AND (n.title LIKE %:keyword% OR n.content LIKE %:keyword%))) " +
+//            "AND (n.category.name = :category)")
     Page<Notice> searchByKeywordAndCategory(String keyword,
                                             String keywordCategory,
                                             String category,
                                             Pageable pageable);
 
     // 키워드만 주어질때
-    @Query("SELECT n FROM Notice n " +
-            "WHERE (:keywordCategory = 'title' AND n.title LIKE %:keyword%) " +
-            "OR (:keywordCategory = 'content' AND n.content LIKE %:keyword%) " +
-            "OR (:keywordCategory = 'author' AND n.author LIKE %:keyword%) " +
-            "OR (:keywordCategory = 'title+content' AND (n.title LIKE %:keyword% OR n.content LIKE %:keyword%))")
+//    @Query("SELECT n FROM Notice n " +
+//            "WHERE (:keywordCategory = 'title' AND n.title LIKE %:keyword%) " +
+//            "OR (:keywordCategory = 'content' AND n.content LIKE %:keyword%) " +
+//            "OR (:keywordCategory = 'author' AND n.author LIKE %:keyword%) " +
+//            "OR (:keywordCategory = 'title+content' AND (n.title LIKE %:keyword% OR n.content LIKE %:keyword%))")
     Page<Notice> searchByKeyword(String keyword,
                                  String keywordCategory,
                                  String category,
                                  Pageable pageable);
 
     //카테고리만 주어질때
-    @Query("SELECT n FROM Notice n " +
-            "WHERE (n.category.name = :category)")
+//    @Query("SELECT n FROM Notice n " +
+//            "WHERE (n.category.name = :category)")
     Page<Notice> searchByCategory(String keyword,
                                   String keywordCategory,
                                   String category,
