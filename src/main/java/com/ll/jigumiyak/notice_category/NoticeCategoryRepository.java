595e25c15ff8d3000d315e91c0a0eb832435b0fd
yup2.dev <126732440+yup2dev@users.noreply.github.com>
2023-08-02 14:58:00 +0900

    Update NoticeCategoryRepository.java

--- src/main/java/com/ll/jigumiyak/notice_category/NoticeCategoryRepository.java
+++ src/main/java/com/ll/jigumiyak/notice_category/NoticeCategoryRepository.java
@@ -1,6 +1,7 @@
 package com.ll.jigumiyak.notice_category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface NoticeCategoryRepository extends JpaRepository<NoticeCategory, Long> {
     Optional<NoticeCategory> findByName(String Name);
