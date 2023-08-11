22379c1c938398edf942029efd704ce590628352
Jserin <jsr3643@naver.com>
2023-08-11 13:21:37 +0900

    test delete

--- src/test/java/com/ll/jigumiyak/JigumiyakApplicationTests.java
+++ src/test/java/com/ll/jigumiyak/JigumiyakApplicationTests.java
@@ -16,36 +16,4 @@ import java.time.LocalDateTime;
 
 @SpringBootTest
 class JigumiyakApplicationTests {
-
-	@Autowired
-	private BoardRepository boardRepository;
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private AddressService addressService;
-
-	@Test
-	@DisplayName("User Signup #1")
-	void t1() {
-		String loginId = "asdf1234";
-		String password = "asdf1234";
-		String email = "asd@asd";
-		Address address = null;
-
-		SiteUser user = this.userService.create(loginId, password, email, address);
-	}
-
-	@Test
-	@DisplayName("User Signup #2")
-	void t2() {
-		String loginId = "asdf12345";
-		String password = "asdf12345";
-		String email = "asdf@asdf";
-		Integer zoneCode = 12345;
-		String mainAddress = "대전 서구";
-		String subAddress = "굿모닝";
-
-		Address address = this.addressService.create(zoneCode, mainAddress, subAddress);
-		SiteUser user = this.userService.create(loginId, password, email, address);
-	}
 }
