acd80d2441c137bbea3bb255a8cf9276527c7135
yup2.dev <126732440+yup2dev@users.noreply.github.com>
2023-08-18 13:34:27 +0900

    Update CommonUtil.java

--- src/main/java/com/ll/jigumiyak/util/CommonUtil.java
+++ src/main/java/com/ll/jigumiyak/util/CommonUtil.java
@@ -1,5 +1,7 @@
 package com.ll.jigumiyak.util;
 
+import com.ll.jigumiyak.file.FileService;
+import com.ll.jigumiyak.file.GenFile;
 import com.ll.jigumiyak.user.SiteUser;
 import com.ll.jigumiyak.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +12,13 @@ import org.springframework.stereotype.Component;
 public class CommonUtil {
 
     private final UserService userService;
+    private final FileService fileService;
 
     public SiteUser usernameToUser(String loginId) {
         return this.userService.getUserByLoginId(loginId);
+    }
+    public String getFilePath(Long id) {
+        GenFile file = this.fileService.getFile(id);
+        return this.fileService.getFilePath(file);
     }
 }
