5630d79f410478be52cd606862a7b9d008ab54e0
Jserin <jsr3643@naver.com>
2023-08-25 03:07:46 +0900

    fix

--- src/main/java/com/ll/jigumiyak/board/BoardController.java
+++ src/main/java/com/ll/jigumiyak/board/BoardController.java
@@ -82,6 +82,7 @@ public class BoardController {
         return String.format("redirect:/board/%d", b.getId());
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
     public String boardModify(BoardForm boardForm, @PathVariable("id") Long id, Principal principal) {
         Board board = this.boardService.getBoard(id);
@@ -94,6 +95,7 @@ public class BoardController {
         return "board_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
     public String boardModify(@Valid BoardForm boardForm, BindingResult bindingResult, Principal principal, @PathVariable("id") Long id) {
         if (bindingResult.hasErrors()) {
