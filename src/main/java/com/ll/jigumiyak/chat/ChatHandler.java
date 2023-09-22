7dcd5403b94023eaaf59da6d5a66dfa6d4ec8765
Jserin <jsr3643@naver.com>
2023-09-22 16:44:11 +0900

    230922

--- src/main/java/com/ll/jigumiyak/chat/ChatHandler.java
+++ src/main/java/com/ll/jigumiyak/chat/ChatHandler.java
@@ -32,8 +32,8 @@ public class ChatHandler extends TextWebSocketHandler {
     // message
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-
-        // json test
+        // websocket으로 수신한 텍스트 메시지 처리
+        // 메세지 데이터 저장
         Map<String, Object> dataMap = new HashMap<>();
 
         // master status
@@ -48,18 +48,20 @@ public class ChatHandler extends TextWebSocketHandler {
         LocalDateTime currentTime = LocalDateTime.now();
         String time = currentTime.format(DateTimeFormatter.ofPattern("hh:mm a, E"));
 
-        // message data
+        // message data 메시지 보낸 아이디와 메시지 내용을 변수에 저장
         String senderId = (String) session.getAttributes().get("sessionId");
         String payload = message.getPayload();
 
         System.out.println("payload >>> " + payload);
 
+        // json문자열을 dataMap으로 변환하고 추가 데이터 저장
         dataMap = jsonToMap(payload);
         dataMap.put("senderId", senderId);
         dataMap.put("time", time);
         dataMap.put("masterStatus", masterStatus);
         dataMap.put("onlineList", onlineList);
 
+        //수신자 id
         String receiverId = (String) dataMap.get("receiverId");
 
         log.info("final dataMap >>> " + dataMap);
@@ -80,28 +82,30 @@ public class ChatHandler extends TextWebSocketHandler {
         }
     }
 
-    // connection established
+    // 웹소켓 연결되고 사용자가 서버 연결되었을 때 메세지 출력
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 
-        // save into session list
+        // 세션 목록에 저장
         String senderId = (String) session.getAttributes().get("sessionId");
         sessionList.add(session);
         log.info("sessionId >>> " + senderId);
         onlineList.add(senderId);
         userSession.put(senderId, session);
 
-        // as master send message to all
+        // 마스터 접속 시 모든 사용자에 메시지 전송
         if (senderId.equals("master")) {
             TextMessage msg = new TextMessage(senderId + " 님이 접속했습니다.");
             sendToAll(msg, senderId);
 
         } else {
+            // 일반 사용자인 경우 마스터에게 접속 메시지 전송
             Map<String, Object> data = new HashMap<>();
             data.put("message", senderId + "님이 접속하셨습니다.");
             data.put("receiverId", "master");
             data.put("newOne", senderId);
 
+            // 메시지를 json 형식으로 바꾸고 마스터에 전송
             TextMessage msgToMaster = new TextMessage(json.writeValueAsString(data));
             handleMessage(session, msgToMaster);
         }
@@ -109,22 +113,24 @@ public class ChatHandler extends TextWebSocketHandler {
         log.info(session + " client connected");
     }
 
-    // connection closed
+    // 연결 종료
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 
+        // 세션목록에서 제거하고 온라인 사용자 목록, 사용자 세션에서 제거
         String senderId = (String) session.getAttributes().get("sessionId");
         sessionList.remove(session);
         onlineList.remove(senderId);
         userSession.remove(senderId);
 
 
-        // as master send to all
+        // 마스터 퇴장시 모든 사용자에게 메시지 전송
         if (senderId.equals("master")) {
             TextMessage msg = new TextMessage(senderId + " 님이 퇴장했습니다.");
             sendToAll(msg, senderId);
 
         } else {
+            // 일반 사용자 퇴장시 마스터에게 전송
             Map<String, Object> data = new HashMap<>();
             data.put("message", senderId + "님이 퇴장하셨습니다.");
             data.put("receiverId", "master");
@@ -133,18 +139,17 @@ public class ChatHandler extends TextWebSocketHandler {
             TextMessage msg = new TextMessage(json.writeValueAsString(data));
             handleMessage(session, msg);
         }
-
-
-
         log.info(session + "client disconnected");
     }
 
     public void getOnlineList() throws IOException {
     }
 
-    // json string parsing to map
+    // json 문자열을 map형식으로 변환
     public Map<String, Object> jsonToMap(String jsonString) throws JsonMappingException, JsonProcessingException {
         Map<String, Object> map = new HashMap<>();
+
+        // ObjectMapper를 사용하여 JSON 문자열을 Map으로 변환
         ObjectMapper jmapper = new ObjectMapper();
         map = jmapper.readValue(jsonString, new TypeReference<Map<String, Object>>() {
         });
@@ -152,9 +157,9 @@ public class ChatHandler extends TextWebSocketHandler {
         return map;
     }
 
-    // send a message to all
+    // 모든 사용자에게 메시지 전송
     public void sendToAll(TextMessage message, String senderId) throws Exception {
-        // json test
+        // map 생성
         Map<String, Object> dataMap = new HashMap<>();
 
         // master status
@@ -188,7 +193,7 @@ public class ChatHandler extends TextWebSocketHandler {
         // send a message
         log.info("receiver session >>> " + userSession.get(receiverId));
 
-        //
+        // 모든 사용자에게 전송
         for (String r : userSession.keySet()) {
             dataMap.put("receiverId", r);
             String msg = json.writeValueAsString(dataMap);
@@ -196,9 +201,11 @@ public class ChatHandler extends TextWebSocketHandler {
         }
     }
 
+    // 온라인 사용자 목록을 클라이언트에 전송
     public void sendOnlineList(WebSocketSession session) throws IOException {
         Map<String, Object> map = new HashMap<>();
-
+        
+        // 온라인 사용자 목록 저장
         map.put("onlineList", onlineList);
         String list = json.writeValueAsString(map);
 
